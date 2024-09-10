d47e3e1c8d0828ad2b1304c1422ec85a314b902e
Amishakul <awadhutkavita@gmail.com>
2024-09-10 17:30:24 +0530

    implemented live api and login button

--- src/App4.js
+++ src/App4.js
@@ -3,14 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 
-
-// const styleCard = {
-//     backgroundColor: "#f0f0f0",
-// };
-
-
 const AppLayout = () => {
-    console.log(<Body/>);
     return (
         <div className="app">
         <Header />
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,17 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../Utils/MockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import resList from "../Utils/MockData";
+import Shimmer from "./Shimmer";
 
 
 const Body = () => {
 
     // Local State Variable - Super Powerful variable
-    const [listofRestaurants, setListOfRestaurant] = useState(resList);
+    //const [listofRestaurants, setListOfRestaurant] = useState(resList);
     // By default we are passing reslist inside listofRestaurants variable
+
+    const [listofRestaurants, setListOfRestaurant] = useState([]);
+    // by default listofrestaurants will be empty
     
     // Normal JS Variable
     //let listofRestaurants = [];
@@ -53,9 +57,45 @@ const Body = () => {
     //     },
         
     //   },]
-    return (
+
+    // useEffect is passed a callback function i.e. arrow function. First the body component will render and then after some miliseconds, this useEffect function will be automatically called(as it is a callback function), and then whatever data is present in this useEffect function will be rendered.
+
+    useEffect(() => {
+        fetchData();
+        //console.log("useEffect Called");
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await data.json();
+        console.log(json);
+        
+        // optional chaining
+         // setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.info);
+
+    };
+
+    // Conditional Rendering: Rendering on the basis of condition
+    // if(listofRestaurants.length === 0) {
+    //     // return <h1>Loading.....</h1>
+    //     return <Shimmer/>
+    // }
+
+    //console.log("Body rendered");
+
+    // conditon rendering using ternary operator
+    return listofRestaurants.length === 0 ? <Shimmer/> : (
         <div className="body">
             <div className="filter">
+            <div className="search">
+                <input type="text" className="search-box" />
+                <button onClick={() => {
+                    // Filter the restaurant cards and update the UI
+                }}>Search</button>
+            </div>
             <button className="filter-btn" onClick={() => {
                 // Filter logic here
                 const filteredList = listofRestaurants.filter((res) => res.data.avgRating > 4);
