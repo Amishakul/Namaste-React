b8100cfbd728f4fef691f8dac3ba905b6ada8fdc
Amishakul <awadhutkavita@gmail.com>
2025-03-02 13:14:56 +0530

    added total price attribute to cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -14,6 +14,18 @@ const Cart = () => {
 
     }
 
+    // Calculate the total price of items in the cart
+    const calculateTotalPrice = () => {
+        return cartItems.reduce((total, item) => {
+            // Check if price exists in the item and calculate accordingly
+            const price = item.card.info.price || item.card.info.defaultPrice;
+            const quantity = item.quantity || 1; // Assuming you have a quantity property
+            return total + (price * quantity) / 100; // Convert to rupees
+        }, 0);
+    };
+
+    const totalPrice = calculateTotalPrice();
+
     
 
     return <div className="text-center m-4 p-4">
@@ -22,6 +34,11 @@ const Cart = () => {
     <div className="py-4"><button className="p-2 m-2 bg-red-600 text-white rounded-lg hover:bg-red-500" onClick={handleClearCart}>Clear Cart</button></div>
     {cartItems.length === 0 && <h1 className="p-4 m-4 font-bold">Cart is empty please add items to the cart !</h1>}
         <ItemList items={cartItems} isCart={true}/>
+        {cartItems.length > 0 && (
+                    <div className="pt-4">
+                        <h2 className="font-bold text-xl">Total: ₹ {totalPrice.toFixed(2)}</h2>
+                    </div>
+                )}
     </div>
     </div>
 };
